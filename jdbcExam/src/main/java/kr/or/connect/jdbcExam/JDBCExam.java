55a7becf0b89e7c26638b16bef975034ea57694b
yeonsoo_lim <yslim6168@naver.com>
2020-07-31 12:06:05 +0900

    200731

---
+++ jdbcExam/src/main/java/kr/or/connect/jdbcExam/JDBCExam.java
@@ -0,0 +1,42 @@
+package kr.or.connect.jdbcExam;
+
+import java.util.List;
+
+import kr.or.connect.jdbcExam.dao.RoleDao;
+import kr.or.connect.jdbcExam.dto.Role;
+
+public class JDBCExam {
+	public static void main(String[] args) {
+		RoleDao dao = new RoleDao();
+				
+//		Role role = dao.getRole(100);
+//		System.out.println(role);
+//		
+//		
+//		
+//		int insertCount = dao.addRole(role);
+//
+//		System.out.println(insertCount);
+//		
+//		
+//		
+//		List<Role> list = dao.getRoles();
+//
+//		for(Role role : list) {
+//			System.out.println(role);
+//		}
+//		
+//		
+//		
+//		int deleteCount = dao.deleteRole(roleId);
+//
+//		System.out.println(deleteCount);
+//		
+//		
+//		
+//		
+//		int updateCount = dao.updateRole(role);
+//
+//		System.out.println(updateCount);
+	}
+}
