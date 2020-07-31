4247351ea4d9663a6e55169e4572c36a0e0b7020
yeonsoo_lim <yslim6168@naver.com>
2020-07-31 12:06:48 +0900

    200731

--- jdbcExam/src/main/java/kr/or/connect/jdbcExam/dao/RoleDao.java
+++ jdbcExam/src/main/java/kr/or/connect/jdbcExam/dao/RoleDao.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import kr.or.connect.jdbcExam.dto.Role;
 
